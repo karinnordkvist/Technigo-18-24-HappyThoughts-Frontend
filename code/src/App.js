956c44a63dd5c17f7f7e0393333e5ef534bc34c4
Karin Nordkvist <kontakt@karinnordkvist.se>
2020-10-28 21:47:24 +0100

    Added footer.

--- code/src/App.js
+++ code/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ListOfThoughts from './components/ListOfThoughts';
+import Footer from './components/Footer';
 
 export const App = () => {
   return (
     <div>
       <ListOfThoughts />
+      <Footer />
     </div>
   );
 };
---
+++ code/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src="../img/heart.png" alt="Heart emoji" className="footer__image" />
+      <p>A website made by Karin Nordkvist @ Technigo - 11/24</p>
+      <p>Copyright 2020 - Icon from Noun Project</p>
+    </footer>
+  );
+};
+
+export default Footer;
